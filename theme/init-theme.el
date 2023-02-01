9cebd3206cc2748a7e3797d2768f7023937e5481
廖承军 <liaochengjun@ticpsh.com>
2023-02-01 18:14:17 +0800

    change theme

--- theme/init-theme.el
+++ theme/init-theme.el
@@ -1,22 +1,25 @@
-(use-package doom-themes)
-(load-theme 'doom-one 1)
+;; (use-package doom-themes)
+;; (load-theme 'doom-one 1)
 
-(use-package valign
-  :ensure t
-  :hook ((markdown-mode org-mode) . valign-mode))
+;; (use-package valign
+;;   :ensure t
+;;   :hook ((markdown-mode org-mode) . valign-mode))
 
-(use-package doom-modeline
-  :ensure t
-  :custom-face
-(mode-line ((t (:height 0.9))))
-(mode-line-inactive ((t (:height 0.9))))
-  :init
-  (doom-modeline-mode t))
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :custom-face
+;; (mode-line ((t (:height 0.9))))
+;; (mode-line-inactive ((t (:height 0.9))))
+;;   :init
+;;   (doom-modeline-mode t))
 
-(use-package visual-fill-column
-  :init
-  ;; Configure fill width
-(setq visual-fill-column-width 110
-      visual-fill-column-center-text t))
+;; (use-package visual-fill-column
+;;   :init
+;;   ;; Configure fill width
+;; (setq visual-fill-column-width 110
+;;       visual-fill-column-center-text t))
+
+(use-package solarized-theme)
+(load-theme 'solarized-light t)
 
 (provide 'init-theme)
