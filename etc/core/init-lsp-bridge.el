2290c72c7f6284bf3b57eb5cefb3c0879e18ddb3
magicalcosmos <liaochengjun@ticpsh.com>
2023-11-22 15:24:56 +0800

    add highlight-matching-tag.el

--- etc/core/init-lsp-bridge.el
+++ etc/core/init-lsp-bridge.el
@@ -4,16 +4,17 @@
 (require 'lsp-bridge)
 (global-lsp-bridge-mode)
 
-(with-eval-after-load 'xref
-  (setq xref-search-program 'ripgrep)     ;project-find-regexp
-  (when (functionp 'xref-show-definitions-completing-read)
-    (setq xref-show-definitions-function #'xref-show-definitions-completing-read)
-    (setq xref-show-xrefs-function #'xref-show-definitions-completing-read)))
+;; (with-eval-after-load 'xref
+;;   (setq xref-search-program 'ripgrep)     ;project-find-regexp
+;;   (when (functionp 'xref-show-definitions-completing-read)
+;;     (setq xref-show-definitions-function #'xref-show-definitions-completing-read)
+;;     (setq xref-show-xrefs-function #'xref-show-definitions-completing-read)))
 
 
   (general-nmap
     :states 'normal
     "gd" '(lsp-bridge-find-def :wk "jump to the definition")
+    "go" '(lsp-bridge-find-def-other-window :wk "jump to the definition in other-window")
     "gt" '(lsp-bridge-popup-documentation :wk " lookup documentation of symbol under the cursor")
     )
 
